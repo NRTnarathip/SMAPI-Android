9010b4cd9b8ae5faf3ebc797adb08090b468ee3d
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-04-15 16:05:41 +0700

    Fix: Support Mod UI Suit Info 2.2.10

--- SMAPI-3.18.6/SMAPI/AndroidExtens/GameRewriter/Game1Facade.cs
+++ SMAPI-3.18.6/SMAPI/AndroidExtens/GameRewriter/Game1Facade.cs
@@ -1,10 +1,10 @@
 ﻿//Rewrite code For PC
 using StardewValley;
+using StardewValley.Menus;
 using System.Collections.Generic;
-
 public class Game1Facade : Game1
 {
     public new static IList<GameLocation> locations => Game1.locations;
-
+    public new static IList<IClickableMenu> onScreenMenus => Game1.onScreenMenus;
 }
 
