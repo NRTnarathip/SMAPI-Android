d9dc7be4038e0943703f1ab7bb6c23b82fdea34d
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-08-31 20:33:29 +0700

    Clean Project

--- SMAPI-3.18.6/SMAPI/Utilities/MethodHelper.cs
+++ SMAPI-3.18.6/SMAPI/Utilities/MethodHelper.cs
@@ -5,5 +5,6 @@ namespace StardewModdingAPI
     public static class MethodHelper
     {
         public static string FullName(this MethodBase method) => $"{method.DeclaringType}::{method}";
+        public static string FullName(this MethodInfo method) => $"{method.DeclaringType}::{method}";
     }
 }
